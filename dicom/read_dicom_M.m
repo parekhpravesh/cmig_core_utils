4d728d2b46f3fa721840aa968d8d8bb91bba8fc9
ChristopherConlin <cconlin@health.ucsd.edu>
2025-09-18 20:47:54 -0700

    Updated read_dicom_M to handle a wider variety of image data (#4)

--- dicom/read_dicom_M.m
+++ dicom/read_dicom_M.m
@@ -19,39 +19,61 @@ if isfield(ref_info, 'PerFrameFunctionalGroupsSequence')
 end
 
 dcminfo_1 = ref_info;
-if ~enhanced_flag
+if ~enhanced_flag % Traditional DICOMs ------------------------------------------------------
 
    PixelSpacing = dcminfo_1.PixelSpacing;
    ImageOrientationPatient = dcminfo_1.ImageOrientationPatient;
    ImagePositionPatient_1 = dcminfo_1.ImagePositionPatient;
-   SpacingBetweenSlices = dcminfo_1.SpacingBetweenSlices;
    
-   ImagePositionPatient = NaN(nfiles, 3);
-   ipp_diff = NaN(nfiles, 3);
-   ipp_diff_norm = NaN(nfiles, 1);
-   for i = 1:nfiles
-     dcminfo = dicominfo(fnames{i});
-     ImagePositionPatient(i,:) = dcminfo.ImagePositionPatient';
-     ipp_diff(i,:) = (dcminfo.ImagePositionPatient - ImagePositionPatient_1)';
-     ipp_diff_norm(i) = norm(ipp_diff(i,:));
+   % Get image volume resolution in slice dimension from DICOM header
+   % Will only be used if it can't be computed from ImagePositionPatient values (e.g., multiframe DICOMs with only one image file)
+   % First check for SpacingBetweenSlices tag. If not present, fall back to SliceThickness
+   if isfield(dcminfo_1, 'SpacingBetweenSlices')
+     SpacingBetweenSlices = dcminfo_1.SpacingBetweenSlices;
+   else
+     SpacingBetweenSlices = dcminfo_1.SliceThickness;
    end
-
-   [unique_ipp, ia, ic] = unique(ImagePositionPatient, 'stable', 'rows');
-   unique_ipp_diff_norm = ipp_diff_norm(ia);
    
-   indx_end = find(unique_ipp_diff_norm==max(unique_ipp_diff_norm));
-   ImagePositionPatient_end = unique_ipp(indx_end,:)';
-   steps_to_end = sum(unique_ipp_diff_norm > 0);
-   step_along_slice = (ImagePositionPatient_end - ImagePositionPatient_1) / steps_to_end;
-   
-else
+   if nfiles > 1
+     ImagePositionPatient = NaN(nfiles, 3);
+     ipp_diff = NaN(nfiles, 3);
+     ipp_diff_norm = NaN(nfiles, 1);
+     for i = 1:nfiles
+       dcminfo = dicominfo(fnames{i});
+       ImagePositionPatient(i,:) = dcminfo.ImagePositionPatient';
+       ipp_diff(i,:) = (dcminfo.ImagePositionPatient - ImagePositionPatient_1)';
+       ipp_diff_norm(i) = norm(ipp_diff(i,:));
+     end
 
+     [unique_ipp, ia, ic] = unique(ImagePositionPatient, 'stable', 'rows');
+     unique_ipp_diff_norm = ipp_diff_norm(ia);
+     
+     indx_end = find(unique_ipp_diff_norm==max(unique_ipp_diff_norm));
+     ImagePositionPatient_end = unique_ipp(indx_end,:)';
+     steps_to_end = sum(unique_ipp_diff_norm > 0);
+     step_along_slice = (ImagePositionPatient_end - ImagePositionPatient_1) / steps_to_end;
+   end   
+
+else % Enhanced DICOMs -----------------------------------------------------------------------
+
+  % Get image volume resolution in slice dimension from DICOM header
+  % Will only be used if it can't be computed from ImagePositionPatient values (e.g., multiframe DICOMs with only one image file)
+  % First check for SpacingBetweenSlices tag. If not present, fall back to SliceThickness
+  % (CCC - Not sure how relevant this is for enhanced DICOMs, and I don't have any example datasets without the SpacingBetweenSlices tag)
   if isfield(dcminfo_1.PerFrameFunctionalGroupsSequence.Item_1,'PixelMeasuresSequence')
     PixelSpacing = dcminfo_1.PerFrameFunctionalGroupsSequence.Item_1.PixelMeasuresSequence.Item_1.PixelSpacing;
-    SpacingBetweenSlices = dcminfo_1.PerFrameFunctionalGroupsSequence.Item_1.PixelMeasuresSequence.Item_1.SpacingBetweenSlices;
+    if isfield(dcminfo_1.PerFrameFunctionalGroupsSequence.Item_1.PixelMeasuresSequence.Item_1, 'SpacingBetweenSlices')
+      SpacingBetweenSlices = dcminfo_1.PerFrameFunctionalGroupsSequence.Item_1.PixelMeasuresSequence.Item_1.SpacingBetweenSlices;
+    else
+      SpacingBetweenSlices = dcminfo_1.PerFrameFunctionalGroupsSequence.Item_1.PixelMeasuresSequence.Item_1.SliceThickness;
+    end
   else
     PixelSpacing = dcminfo_1.SharedFunctionalGroupsSequence.Item_1.PixelMeasuresSequence.Item_1.PixelSpacing;
-    SpacingBetweenSlices = dcminfo_1.SharedFunctionalGroupsSequence.Item_1.PixelMeasuresSequence.Item_1.SpacingBetweenSlices;
+    if isfield(dcminfo_1.SharedFunctionalGroupsSequence.Item_1.PixelMeasuresSequence.Item_1, 'SpacingBetweenSlices')
+      SpacingBetweenSlices = dcminfo_1.SharedFunctionalGroupsSequence.Item_1.PixelMeasuresSequence.Item_1.SpacingBetweenSlices;
+    else
+      SpacingBetweenSlices = dcminfo_1.SharedFunctionalGroupsSequence.Item_1.PixelMeasuresSequence.Item_1.SliceThickness;
+    end
   end
 
   ImageOrientationPatient = dcminfo_1.PerFrameFunctionalGroupsSequence.Item_1.PlaneOrientationSequence.Item_1.ImageOrientationPatient;
@@ -85,11 +107,13 @@ col_orientation = ImageOrientationPatient(4:6);
 step_along_row = PixelSpacing(2) * row_orientation;
 step_along_col = PixelSpacing(1) * col_orientation;
 
-if ~exist('step_along_slice', 'var') && exist('gfov', 'var')
-  step_along_slice = (gfov(2)-gfov(1)) * slice_orientation;
-elseif ~exist('step_along_slice', 'var')
+if ~exist('step_along_slice', 'var') % Only a single file/frame
   slice_orientation = cross(row_orientation, col_orientation);
-  step_along_slice = SpacingBetweenSlices * slice_orientation;
+  if exist('gfov', 'var')
+    step_along_slice = (gfov(2)-gfov(1)) * slice_orientation;
+  else
+    step_along_slice = SpacingBetweenSlices * slice_orientation;
+  end
 end
 
 M = eye(4);
